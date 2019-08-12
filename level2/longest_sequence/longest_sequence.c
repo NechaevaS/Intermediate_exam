7d0765f30a6aa3f5f68e9eac8f18f87e5b229866
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-08-12 16:26:17 -0700

    checked in practice mode

--- level2/longest_sequence/longest_sequence.c
+++ level2/longest_sequence/longest_sequence.c
@@ -6,39 +6,40 @@
 /*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 14:02:34 by snechaev          #+#    #+#             */
-/*   Updated: 2019/07/12 15:41:08 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/08/12 16:25:54 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-struct		s_node 
+struct s_node
 {
 	int value;
-	struct s_node	*left;
-	struct s_node	*right;
+	struct s_node *left;
+	struct s_node *right;
 };
 
-void	check_path(struct s_node *node, int curr_path, int *max_path, int pr_val)
+void count_node(struct s_node *node, int *max, int curr, int pr_val)
 {
 	if (!node)
 		return ;
-	if (node->value - 1 == pr_val)
-		curr_path++;
+	if (node->value == pr_val + 1)
+		curr++;
 	else
-		curr_path = 1;
-	if (curr_path > *max_path)
-		*max_path = curr_path;
-	check_path(node->left, curr_path, max_path, node->value);
-	check_path(node->right, curr_path, max_path, node->value);
+		curr = 1;
+	if (curr > *max)
+		*max = curr;
+	count_node(node->left, max, curr, node->value);
+	count_node(node->right, max, curr, node->value);
 }
 
 int	longest_sequence(struct s_node *node)
 {
-	int max_path = 1;
+	int max = 1;
 
 	if (!node)
 		return (0);
-
-	check_path(node->left, 1, &max_path, node->value);
-	check_path(node->right, 1, &max_path, node->value);
-	return (max_path);
+	if(node->left)
+		count_node(node->left, &max, 1, node->value);
+	if(node->right)
+		count_node(node->right, &max, 1, node->value);
+	return (max);
 }
