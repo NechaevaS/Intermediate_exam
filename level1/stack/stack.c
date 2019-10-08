b732ba83e909315a8a3894cb993a0291d2c0fccf
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-10-08 15:40:57 -0700

    checked

--- level1/stack/stack.c
+++ level1/stack/stack.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/11 15:43:30 by snechaev          #+#    #+#             */
-/*   Updated: 2019/07/30 15:54:32 by snechaev         ###   ########.fr       */
+/*   Created: 2019/09/24 09:26:07 by exam              #+#    #+#             */
+/*   Updated: 2019/09/24 09:26:08 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 struct s_node
 {
-	void *content;
+	void          *content;
 	struct s_node *next;
 };
 
 struct s_stack
 {
-	struct s_node *top;
+    struct s_node *top;
 };
 
 struct s_stack *init(void);
@@ -35,50 +35,50 @@ int isEmpty(struct s_stack *stack);
 
 struct s_stack *init(void)
 {
-	struct s_stack *stack;
+    struct s_stack *stack;
 
-	stack = (struct s_stack *)malloc(sizeof(struct s_stack));
-	stack->top = NULL;
-	return (stack);
+    stack = (struct s_stack *)malloc(sizeof(struct s_stack));
+    stack->top = NULL;
+    return (stack);
 }
 
 int isEmpty(struct s_stack *stack)
 {
-	if (!stack || stack->top == NULL)
-		return (1);
-	return (0);
+    if (!stack || !stack->top)
+        return (1);
+    return (0);
 }
 
 void push(struct s_stack *stack, void *content)
 {
-	struct s_node *item;
+    struct s_node *elem;
 
-	if (!stack)
-		return;
-	item = (struct s_node *)malloc(sizeof(struct s_node));
-	item->content = content;
-	item->next = stack->top;
-	stack->top = item;
+    if (!stack)
+        return ;
+    elem = (struct s_node *)malloc(sizeof(struct s_node));
+    elem->content = content;
+    elem->next = NULL;
+    elem->next = stack->top;
+    stack->top = elem;
 }
 
 void *pop(struct s_stack *stack)
 {
-	void *data;
-	struct s_node *tmp;
+    struct s_node   *tmp;
+    void            *data;
 
-	if (isEmpty(stack))
-		return (NULL);
-	data = stack->top->content;
-	tmp = stack->top;
-	stack->top = tmp->next;
-	free(tmp);
-	return (data);
+    if (isEmpty(stack))
+        return (NULL);
+    tmp = stack->top;
+    data = tmp->content;
+    stack->top = tmp->next;
+    free(tmp);
+    return (data);
 }
 
 void *peek(struct s_stack *stack)
 {
-	if (isEmpty(stack))
-		return (NULL);
-	return (stack->top->content);
+    if (isEmpty(stack))
+        return (NULL);
+    return (stack->top->content);
 }
-
