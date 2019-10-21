966e288a4f5fd273b0d9a9b91974a5d45827ab94
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-10-21 16:53:14 -0700

    checked by exam

--- level5/infin_mult/infin_mult.c
+++ level5/infin_mult/infin_mult.c
@@ -1,148 +1,141 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   infin_mult.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snechaev <snechaev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 16:22:51 by snechaev          #+#    #+#             */
+/*   Updated: 2019/10/21 16:22:55 by snechaev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
+#include <unistd.h>
 
 int str_len(char *s)
 {
     int i = 0;
-
-    while (s[i])
+    while(s[i])
     {
         i++;
     }
     return (i);
 }
 
-void zero_arr(int *s, int n)
+void zero_arr(int len, int *arr)
 {
     int i = 0;
-
-    while(i < n)
+    while(i < len)
     {
-        s[i] = 0;
+        arr[i] = 0;
         i++;
     }
 }
 
-void print(int *s, int n)
+void parse(char *s, int len, int max, int arr[max])
 {
     int i = 0;
-    char c;
+    int j = len - 1;
 
-    while(i < n)
-    {
-        c = s[i] + '0';
-        write(1, &c, 1);
-        i++;
-    }
-    write(1, "\n", 1);
-}
-
-int *create_rev_arr(char *s, int l1, int len)
-{
-    int i = 0;
-    int j;
-    int *arr;
-
-    j = l1-1;
-    arr = (int *)malloc(sizeof(int) * len);
-    zero_arr(arr, len);
-    while(i < len && j >= 0)
+    zero_arr(max, arr);
+    while(i < max && j >= 0)
     {
         arr[i] = (s[j] - '0');
         i++;
         j--;
     }
-    return (arr);
 }
 
-int *multipl(int *a1, int *a2, int len)
+void print_arr(int len, int arr[len], int sign1, int sign2)
 {
-    int in_m;
-    int mult;
-    int *res;
-    int pos;
+    char c;
+    int i;
+
+    if (sign1 || sign2)
+    {
+        if (sign1 != sign2)
+            write(1, "-", 1);
+    }
+    i = len - 1;
+    while(arr[i] == 0)
+        i--;
+    while(i >= 0)
+    {
+        c = arr[i] + '0';
+        write(1, &c, 1);
+        i--;
+    }
+    write(1, "\n", 1);
+}
+
+void mult(int len_res, int len, int arr1[len], int arr2[len], int res[len_res])
+{
     int i1 = 0;
     int i2;
+    int car = 0;
+    int mult;
+    int pos;
 
-    res = (int *)malloc(sizeof(int) * len * 2);
-    zero_arr(res, len * 2);
+    zero_arr(len_res, res);
     while(i1 < len)
     {
-        in_m = 0;
+        car = 0;
         i2 = 0;
         while(i2 < len)
         {
             pos = i1 + i2;
-            mult = a1[i1] * a2[i2] + in_m + res[pos];
-            res[pos] = mult % 10;
-            in_m = mult / 10;
+            mult = arr1[i1] * arr2[i2] + car + res[pos];
+            res[pos] =  mult % 10;
+            car = mult / 10;
             i2++;
         }
-        res[pos + 1] = res[pos + 1] + in_m;
+        res[pos + 1] = res[pos + 1] + car;
         i1++;
     }
-    return (res);
-}
-
-void print_arr(int *arr, int n, int sign)
-{
-    int i;
-    char c;
-
-    i = n - 1;
-    if (sign)
-        write (1, "-", 1);
-    while (arr[i] == 0)
-    {
-        i--;
-    }
-    while (i >= 0)
-    {
-        c = arr[i] + '0';
-        write (1, &c, 1);
-        i--;
-    }  
 }
 
 int main(int argc, char **argv)
 {
-    int *a1;
-    int *a2;
-    int l1;
-    int l2;
-    int len;
-    int *mult;
-    int sign = 0;
-    
-    if (argc == 3)
+    int sign1 = 0;
+    int sign2 = 0;
+    int len1;
+    int len2;
+    int len_max;
+
+    if (argc != 3)
+        return (write(1, "\n", 1));
+    len1 = str_len(argv[1]);
+    len2 = str_len(argv[2]);
+    if (!len1 || !len2)
+        return (write(1, "\n", 1));
+    if (argv[1][0] == '0' || argv[2][0] == '0')
     {
-        l1 = str_len(argv[1]);
-        l2 = str_len( argv[2]);
-        if (argv[1][0] == '-')
-        {
-            sign = 1;
-            l1--;
-            argv[1]++;
-        }
-        if (argv[2][0] == '-')
-        {
-            sign = 1;
-            l2--;
-            argv[2]++;
-        }
-        if (argv[1][0] == '0' || argv[2][0] == '0')
-        {
-            write (1, "0\n", 2);
-            return (0);
-        }
-        if (l1 > l2)
-            len = l1;
-        else
-            len = l2;
-        a1 = create_rev_arr(argv[1], l1, len);
-        a2 = create_rev_arr(argv[2], l2, len);
-        mult = multipl(a1, a2, len);
-        print_arr(mult, len * 2, sign);
+        return (write(1, "0\n", 2));
+        return (0);
     }
-    write(1, "\n", 1);
+    if (argv[1][0] == '-')
+    {
+        sign1 = 1;
+        argv[1]++;
+        len1--;
+    }
+    if (argv[2][0] == '-')
+    {
+        sign2 = 1;
+        argv[2]++;
+        len2--;
+    }
+    if (len1 > len2)
+        len_max = len1;
+    else
+        len_max = len2;
+    int arr1[len_max];
+    int arr2[len_max];
+    int res[len1 + len2];
+    parse(argv[1], len1, len_max, arr1);
+    parse(argv[2], len2, len_max, arr2);
+    mult(len1 + len2, len_max, arr1, arr2, res);
+    print_arr(len1 + len2, res, sign1, sign2);
+    return (0);
 }
